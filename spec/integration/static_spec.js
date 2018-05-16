7474990788dd60d1044e5971cd0278175dcb428a
Ryan Noronha <ryannoronha1@Ryans-MacBook-Pro.local>
2018-05-16 16:50:24 -0400

    Assignment 2 completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -16,4 +16,26 @@ describe("routes : static", () => {
     });
 
   });
+
+  describe("GET /marco", () => {
+
+    it("should return status code 200", (done) => {
+
+      request.get('http://localhost:3000/marco', (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+
+        done();
+      });
+    });
+
+    it("should return string 'Polo' ", (done) => {
+
+      request.get('http://localhost:3000/marco', (err, res, body) => {
+        expect(body).toContain('Polo');
+
+        done();
+      });
+    });
+
+  });
 });
