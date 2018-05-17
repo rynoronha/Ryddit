7441ad43e62335e3ca7da35f154be08c8ba5c46b
Ryan Noronha <ryannoronha1@Ryans-MacBook-Pro.local>
2018-05-17 18:20:37 -0400

    updated server code, reset to previous version

--- src/server.js
+++ src/server.js
@@ -1,9 +1,8 @@
 const app = require("./app");
 const http = require("http");
-const server = http.createServer(app);
 
-server.listen(3000);
+const port = process.env.PORT || 3000;
 
-server.on("listening", () => {
-  console.log("server is listening for requests on port 3000");
+app.listen(port, () => {
+  console.log(`App is running on port ${port}...`);
 });
