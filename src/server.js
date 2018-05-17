6ae336b7db666e009e03f50f103570c3257bfa45
Ryan Noronha <ryannoronha1@Ryans-MacBook-Pro.local>
2018-05-17 17:46:04 -0400

    update 2

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const app = require("./app");
 const http = require("http");
 
-const port = normalizePort(process.env.PORT || "3000");
-app.set("port", port);
+var port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function() {
+console.log("Listening on Port 3000");
+});
 
 const server = http.createServer(app);
 
