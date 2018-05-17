ad3f95fb8efbdb91a8516c71a8e2704fbc601b8f
Ryan Noronha <ryannoronha1@Ryans-MacBook-Pro.local>
2018-05-17 18:07:13 -0400

    update 3

--- src/server.js
+++ src/server.js
@@ -1,26 +1,8 @@
 const app = require("./app");
 const http = require("http");
 
-var port = process.env.PORT || 3000;
-app.listen(port, "0.0.0.0", function() {
-console.log("Listening on Port 3000");
-});
+const port = process.env.PORT || 3000;
 
-const server = http.createServer(app);
-
-server.listen(port);
-
-function normalizePort(val) {
- const port = parseInt(val, 10);
-  if (isNaN(port)) {
-    return val;
-  }
-  if (port >= 0) {
-    return port;
-  }
-  return false;
-}
-
-server.on("listening", () => {
-  console.log(`server is listening for requests on port ${server.address().port}`);
+app.listen(port, () => {
+  console.log(`App is running on port ${port}...`);
 });
