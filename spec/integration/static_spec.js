2832bde557bc3f2a3974cebac529ef20cd6317f3
Ryan Noronha <ryannoronha1@Ryans-MacBook-Pro.local>
2018-05-18 16:33:21 -0400

    server working, need to fix test failure

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -21,7 +21,7 @@ describe("routes : static", () => {
 
     it("should return status code 200 and have 'About Us' in the body of the response", () => {
 
-      request.get(base/about, (err, res, body) => {
+      request.get('http://localhost:3000/about', (err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(body).toContain("About Us");
 
--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -1,8 +1,8 @@
 module.exports = {
   index(req, res, next){
     res.render("static/index", {title: "Welcome to Bloccit"});
-  };
+  },
   about(req, res, next){
-    res.render("static/partials/about");
-  };
+    res.render("static/about");
+  }
 }
